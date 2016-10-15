b01cf22772158ce7c950c206e2a7b8aa07d5d2a1
Ajay Vishwanath <aj.vish@outlook.com>
2016-10-15 14:40:04 +0800

    Train HMM with 12 data points

--- readData.py
+++ readData.py
@@ -1,39 +1,140 @@
 import PIL.Image
 import cv2
 import xml.etree.ElementTree as ET
+import os
 import numpy as np
 from seqlearn.datasets import load_conll
 from seqlearn.hmm import MultinomialHMM
 from seqlearn.evaluation import bio_f_score
 
-# The feature extractor: So far does nothing but splits the pixels of the image by spaces
+# The feature extractor: So far does nothing but splits the pixels of the
+# image by spaces
+
+
+def list_files(dir):
+    r = []
+    subdirs = [x[0] for x in os.walk(dir)]
+    for subdir in subdirs:
+        files = os.walk(subdir).next()[2]
+        if (len(files) > 0):
+            for file in files:
+                r.append(subdir + "/" + file)
+    return r
+
+
 def features(sequence, i):
     split_sequence = sequence[i].split(" ")
     # print i
-    for pixel in split_sequence:
-    	# print pixel + "lol"
-    	yield pixel
+    print len(split_sequence)
+    yield str(sequence)
 
+
+IMAGE_DIRECTORY = "Datasets/sentences/a01/a01-000x/"
+my_images = []
 # Reads image from dataset and stores it as a numpy array
-myimage = cv2.imread("Datasets/sentences/a01/a01-000u/a01-000u-s00-00.png", 0)
+for i in range(0, 2):
+    for j in range(0, 3):
+        myimage = cv2.imread(
+            IMAGE_DIRECTORY + "a01-000x-s0" + str(i) +
+            "-0" + str(j) + ".png", 0)
+        (thresh, im_bw) = cv2.threshold(myimage, 128,
+                                        255, cv2.THRESH_BINARY |
+                                        cv2.THRESH_OTSU)
+        my_images.append(im_bw / 255)
+myimage = cv2.imread(
+    IMAGE_DIRECTORY + "a01-000x-s01-03.png", 0)
+(thresh, im_bw) = cv2.threshold(myimage, 128,
+                                255, cv2.THRESH_BINARY |
+                                cv2.THRESH_OTSU)
+my_images.append(im_bw / 255)
+# cv2.imwrite("bw.jpg", im_bw)
+print len(my_images)
 
 # Captures the jpeg file and extracts METADATA
-jpgfile = PIL.Image.open('Datasets/sentences/a01/a01-000u/a01-000u-s00-00.png')
+jpgfile = PIL.Image.open(IMAGE_DIRECTORY + "/a01-000x-s01-00.png")
 
 # The METADATA is an XML, hence parsing
 myxml = ET.fromstring(jpgfile.info["Description"])
 
+print jpgfile.info["Description"]
+
 # Gets the text corresponding to the file a01-000u-s00-00.png
 sentence = myxml.find("handwritten-part")[0].attrib["text"]
 
-# Saves the image into a TXT file
-np.savetxt("test.txt", myimage, fmt="%1.2f", newline=" ",
-           header='[', footer='] [' + sentence + ']', comments='')
+lines = myxml.findall("./handwritten-part/line")
+i = 0
+line_mapping = [[], [], [], [], [], [], [], [], []]
+for line in lines:
+    lower = line.find("./lower-contour")
+    upper = line.find("./upper-contour")
+    my_image = my_images[i]
+    print "image shape: ", my_image.shape
+    x1 = []
+    x2 = []
+    if lower is not None:
+        for point in lower.findall("./point"):
+            x1.append(int(point.attrib["x"]))
+    if upper is not None:
+        for point in upper.findall("./point"):
+            x2.append(int(point.attrib["x"]))
+    words = line.findall("./word")
+    for word in words:
+        print "The Word: ****", word.attrib["text"], "****"
+        word_width = 0
+        x = int(word.findall("./cmp")[0].attrib["x"])
+        print "starts at ", x
+        for ch in word.findall("./cmp"):
+            word_width += int(ch.attrib["width"])
+        print "word width: ", word_width
+        if len(x1) == 0:
+            print "minimum = ", x2[0]
+            offset = x - x2[0]
+        else:
+            print "minimum = ", x1[0]
+            offset = x - x1[0]
+        if offset <= 0:
+            offset = 0
+        # if my_image.shape[1] < offset:
+        #     offset = offset - my_image.shape[1]
+        #     i += 1
+        #     my_image = my_images[i]
+        print "offset ", offset
+        word_image = my_image[
+            :, offset:offset + word_width]
+        # print word_image.shape
+        # if word_image.shape[1] == 0:
+        #     print "EMPTYYYYYYYYYYYYYYYYYYYY"
+        my_word = {
+            "matrix": word_image,
+            "word": word.attrib["text"]
+        }
+        line_mapping[i].append(my_word)
+        # cv2.imwrite("line" + str(i) + "-" +
+        #             str(len(line_mapping[i]) - 1) + "-" +
+        #             my_word["word"] + ".jpg",
+        #             my_word["matrix"] * 255)
+        print my_word["matrix"].shape
+    print '\n'
+    i += 1
 
-# Extracts features from the datasets
+
+# Saves the image into a TXT file
+for line in line_mapping:
+    for word in line:
+        if word["matrix"].shape[1] == 0:
+            print "Zero matrix... Skipping..."
+        else:
+            f_handle = file('test.txt', 'a')
+            np.savetxt(f_handle, word['matrix'], delimiter=" ",
+                       fmt="%i", newline=" ",
+                       header='', footer="" + word["word"] + "\n\n",
+                       comments='')
+            f_handle.close()
+
+# # Extracts features from the datasets
 X_train, y_train, lengths_train = load_conll("test.txt", features)
 
-# Models it as an HMM
+# # Models it as an HMM
 clf = MultinomialHMM()
 clf.fit(X_train, y_train, lengths_train)
 
@@ -43,5 +144,6 @@ print X_train, y_train
 X_test, y_test, lengths_test = load_conll("test.txt", features)
 y_pred = clf.predict(X_test, lengths_test)
 
-# Final score
-print(bio_f_score(y_test, y_pred))
+print y_pred
+# # Final score
+# print(bio_f_score(y_test, y_pred))
